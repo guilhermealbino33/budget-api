b5ac6feb110b715e4011bb6201f6c00a43568116
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-08-30 11:00:46 -0300

    Update index.ts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,16 +2,12 @@ import { container } from "tsyringe";
 
 import { IUsersRepository } from "../../modules/users/repositories/IUsersRepository";
 import { UsersRepository } from "../../modules/users/repositories/implementations/UsersRepository";
-import { CreateUserService } from "../../modules/users/services/createUser.service";
-import ShowUserProfileService from "../../modules/users/services/showUserProfile.service";
-import AuthenticateUserService from "../../modules/users/services/authenticateUser.service";
+import IUsersService from "../../modules/users/services/IUsersService";
+import UsersService from "../../modules/users/services/implementations/users.service";
 
+// Users
+container.registerSingleton<IUsersService>("UsersService", UsersService);
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
-
-// Users
-container.registerSingleton("CreateUserService", CreateUserService);
-container.registerSingleton("ShowUserProfileService", ShowUserProfileService);
-container.registerSingleton("AuthenticateUserService", AuthenticateUserService);
