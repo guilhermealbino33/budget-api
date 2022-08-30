08e3dbcfd823674d5283df151159db361b802475
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-08-30 11:00:17 -0300

    refactor: users repository

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,7 +2,7 @@ import { User } from "../../../entities/User";
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 
 export interface IUsersRepository {
-  create: (data: ICreateUserDTO) => Promise<User>;
-  findByEmail: (email: string) => Promise<User | undefined>;
-  findById: (user_id: string) => Promise<User | undefined>;
+  create(data: ICreateUserDTO): Promise<User>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findById(user_id: string): Promise<User | undefined>;
 }
