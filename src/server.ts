afbd9eaa5435e1a6a6b1b61201f89ce03e384f88
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-08-31 21:28:57 -0300

    feat: comment database call

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,39 @@
-import { app } from "./app";
+import 'reflect-metadata';
+import 'express-async-errors';
 
-app.listen(3333, () => {
-  console.log("Server is running");
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+import createDbConnection from './database';
+import './shared/container';
+import { router } from './routes';
+import { AppError } from './shared/errors/AppError';
+
+// createDbConnection();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1', router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+const port = 3033;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
