6bc4048f44ce7e86f5caede40d66f0caf3cbb807
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-11 11:47:04 -0300

    feat: add CreateBudgetProductsUseCase and CreateBudgetAdditionalItemsUseCase

--- src/modules/budgets/controllers/budget.controller.ts
+++ src/modules/budgets/controllers/budget.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import CreateBudgetUseCase from '../useCases/createBudget.useCase';
+import CreateBudgetAdditionalItemsUseCase from '../useCases/createBudgetAdditionaltems.useCase';
+import CreateBudgetProductsUseCase from '../useCases/createBudgetProducts.useCase';
 import DeleteBudgetUseCase from '../useCases/deleteBudget.useCase';
 import ShowBudgetUseCase from '../useCases/showBudget.useCase';
 import UpdateBudgetUseCase from '../useCases/updateBudget.useCase';
@@ -11,26 +13,29 @@ export async function createBudgetHandler(
 ) {
   const {
     code,
-    customer,
+    customer_id,
     products,
-    salesman,
+    salesman_id,
     quantity,
     delivery_type,
     delivery_value,
     observations,
     additional_items,
+    total_value,
   } = request.body;
+
   const createBudgetUseCase = container.resolve(CreateBudgetUseCase);
   const budget = await createBudgetUseCase.execute({
     code,
-    customer,
+    customer_id,
     products,
-    salesman,
+    salesman_id,
     quantity,
     delivery_type,
     delivery_value,
     observations,
     additional_items,
+    total_value,
   });
 
   return response.status(201).json(budget);
@@ -85,6 +90,42 @@ export async function showBudgetHandler(request: Request, response: Response) {
   const { id } = request.params;
   const showBudgetUseCase = container.resolve(ShowBudgetUseCase);
   const budget = await showBudgetUseCase.execute(id);
+
+  return response.status(200).json(budget);
+}
+
+export async function createBudgetProductsHandler(
+  request: Request,
+  response: Response
+) {
+  const { id } = request.params;
+  const { product_ids } = request.body;
+
+  const createBudgetProductsUseCase = container.resolve(
+    CreateBudgetProductsUseCase
+  );
+  const budget = await createBudgetProductsUseCase.execute({
+    budget_id: id,
+    product_ids,
+  });
+
+  return response.status(200).json(budget);
+}
+
+export async function createBudgetAdditionalItemsHandler(
+  request: Request,
+  response: Response
+) {
+  const { id } = request.params;
+  const { additional_items_ids } = request.body;
+
+  const createBudgetAdditionalItemsUseCase = container.resolve(
+    CreateBudgetAdditionalItemsUseCase
+  );
+  const budget = await createBudgetAdditionalItemsUseCase.execute({
+    budget_id: id,
+    additional_items_ids,
+  });
 
   return response.status(200).json(budget);
 }
--- src/modules/budgets/useCases/createBudget.useCase.ts
+++ src/modules/budgets/useCases/createBudget.useCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from 'tsyringe';
+import { IAdditionalItem } from '../../../entities/additionalItem';
 import { IBudget } from '../../../entities/budget';
+import { IProduct } from '../../../entities/product';
 import { AppError } from '../../../shared/errors/AppError';
 import { IBudgetsRepository } from '../repositories/IBudgetsRepository';
 
@@ -19,6 +21,29 @@ export default class CreateBudgetUseCase {
       throw new AppError('Budget already exists!', 409);
     }
 
+    budget.total_value = await this.calculateTotalValue(
+      budget.products,
+      budget.additional_items
+    );
+
     await this.budgetsRepository.create(budget);
+  }
+
+  private async calculateTotalValue(
+    products: IProduct[],
+    additionalItems?: IAdditionalItem[]
+  ): Promise<number> {
+    const sum = [];
+    const productsValue = products.map((product) => product.value);
+    sum.push(...productsValue);
+
+    if (additionalItems) {
+      const additionalItemsValue = additionalItems.map(
+        (additionalItem) => additionalItem.value
+      );
+      sum.push(...additionalItemsValue);
+    }
+
+    return sum.reduce((partialSum, a) => partialSum + a, 0);
   }
 }
