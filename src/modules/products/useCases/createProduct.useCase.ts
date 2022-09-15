1d6dfc2ba4c73708053403859912afe7b28bb82b
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-14 21:20:03 -0300

    feat: add validations on create product

--- src/modules/products/useCases/createProduct.useCase.ts
+++ src/modules/products/useCases/createProduct.useCase.ts
@@ -1,13 +1,16 @@
 import { inject, injectable } from 'tsyringe';
 import { IProduct } from '../../../entities/product';
 import { AppError } from '../../../shared/errors/AppError';
+import { ICategoriesRepository } from '../../categories/repositories/ICategoriesRepository';
 import { IProductsRepository } from '../repositories/IProductsRepository';
 
 @injectable()
 export default class CreateProductUseCase {
   constructor(
     @inject('ProductsRepository')
-    private productsRepository: IProductsRepository
+    private productsRepository: IProductsRepository,
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository
   ) {}
 
   async execute(product: IProduct) {
@@ -17,6 +20,18 @@ export default class CreateProductUseCase {
 
     if (productAlreadyExists) {
       throw new AppError('Product already exists!', 409);
+    }
+
+    if (!product.category_id) {
+      throw new AppError('Product must belong to a category!', 400);
+    }
+
+    const category = await this.categoriesRepository.findById(
+      product.category_id
+    );
+
+    if (!category) {
+      throw new AppError('Category not found!', 404);
     }
 
     await this.productsRepository.create(product);
