874b38efa514daad2f1b0b5cff6df84376eefd5d
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-21 22:14:03 -0300

    Create deleteProductImage.useCase.ts

---
+++ src/modules/products/useCases/deleteProductImage.useCase.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../shared/errors/AppError';
+import { IStorageProvider } from '../../../shared/providers/StorageProvider/IStorageProvider';
+import { IProductsImagesRepository } from '../repositories/IProductsImagesRepository';
+import { IProductsRepository } from '../repositories/IProductsRepository';
+
+@injectable()
+export default class DeleteProductImageUseCase {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+    @inject('ProductsImagesRepository')
+    private productsImagesRepository: IProductsImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
+  ) {}
+
+  async execute(id: string, imagesName: string[]) {
+    if (!id) {
+      throw new AppError('Product id must be informed!', 400);
+    }
+
+    const product = await this.productsRepository.findById(id);
+
+    if (!product) {
+      throw new AppError('Product not found!', 404);
+    }
+
+    imagesName.map(async (image) => {
+      await this.productsImagesRepository.create(id, image);
+      await this.storageProvider.delete(image, 'products');
+    });
+  }
+}
