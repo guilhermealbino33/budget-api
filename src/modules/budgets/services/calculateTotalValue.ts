13059e435a051f2c841ce238f1104001d8cdcaae
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-27 22:09:08 -0300

    feat: calculate total value

--- src/modules/budgets/services/calculateTotalValue.ts
+++ src/modules/budgets/services/calculateTotalValue.ts
@@ -1,30 +1,29 @@
-import { IBudget } from '../../../entities/budget';
-import { IBudgetAdditionalItems } from '../../../entities/budgetAdditionalItems';
-import { IBudgetProducts } from '../../../entities/budgetProducts';
+import { Budget } from '../../../entities/budget';
 
-export async function calculateTotalValue(budget: IBudget): Promise<number> {
+export async function calculateTotalValue(budget: Budget): Promise<number> {
   const sum = [];
-  const productsValue = budget.products.map((product) =>
-    calculateProductTotalPrice(product)
-  );
+  const productsValue = budget.products.map((product) => product.total_price);
+
   sum.push(...productsValue);
 
   if (budget.additional_items) {
-    const additionalItemsValue = budget.additional_items.map((additionalItem) =>
-      calculateProductTotalPrice(additionalItem)
+    const additionalItemsValue = budget.additional_items.map(
+      (additional_item) => additional_item.total_price
     );
     sum.push(...additionalItemsValue);
   }
 
   if (budget.delivery_value) {
-    sum.push(budget.delivery_value);
+    sum.push(Number(budget.delivery_value));
   }
 
   return sum.reduce((partialSum, a) => partialSum + a, 0);
 }
 
 export function calculateProductTotalPrice(
-  item: IBudgetProducts | IBudgetAdditionalItems
+  unit_price: number,
+  quantity: number,
+  discount = 0
 ): number {
-  return item.unit_price * item.quantity - item.discount;
+  return unit_price * quantity - discount;
 }
