cee7e8a8918afc65b296dd7726eeb5f45ee3c1e8
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-10-09 10:53:16 -0300

    feat: token leasts 365d

--- src/shared/config/auth.ts
+++ src/shared/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: process.env.SECRET_TOKEN,
-  expires_in_token: '1d',
+  expires_in_token: '365d',
   expires_in_refresh_token: '365d',
   secret_refresh_token: process.env.REFRESH_TOKEN,
   expires_refresh_token_days: 365,
