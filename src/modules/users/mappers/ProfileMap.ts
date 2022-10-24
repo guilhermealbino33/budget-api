d0892e0f6a60cd97823eccac5ccd43e36a7870ac
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-10-24 11:36:22 -0300

    feat: add role to get users

--- src/modules/users/mappers/ProfileMap.ts
+++ src/modules/users/mappers/ProfileMap.ts
@@ -1,11 +1,12 @@
 import { User } from '../../../entities/user';
 
 export class ProfileMap {
-  static toDTO({ id, name, email, created_at, updated_at }: User) {
+  static toDTO({ id, name, email, role, created_at, updated_at }: User) {
     return {
       id,
       name,
       email,
+      role,
       created_at,
       updated_at,
     };
